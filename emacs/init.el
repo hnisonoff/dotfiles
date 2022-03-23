f740e4d66d0dd5ff0b2e4e65833e49991512d124
Hunter Nisonoff <hunter@octant.bio>
2022-03-23 12:46:11 -0700

    Updated emacs

--- emacs/init.el
+++ emacs/init.el
@@ -6,44 +6,4 @@
 
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
-;; (custom-set-variables
-;;  ;; custom-set-variables was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  '(conda-anaconda-home (expand-file-name "~/anaconda3/"))
-;;  '(conda-env-home-directory (expand-file-name "~/anaconda3/"))
-;;  '(org-capture-templates
-;;    '(("b" "Add book to read" entry
-;;       (file+headline "~/.emacs.d/orgfiles/private.org" "Books to read")
-;;       (file "~/.emacs.d/orgfiles/tpl-book.txt")
-;;       :empty-lines-after 2)
-;;      ("p" "Add paper to read" entry
-;;       (file+headline "~/.emacs.d/orgfiles/work.org" "Papers To Read")
-;;       (file "~/.emacs.d/orgfiles/tpl-papers.txt"))
-;;      ("i" "Add research idea" entry
-;;       (file+headline "~/.emacs.d/orgfiles/work.org" "Research Ideas")
-;;       (file "~/.emacs.d/orgfiles/tpl-idea.txt"))
-;;      ("t" "Add todo" entry
-;;       (file "~/.emacs.d/orgfiles/capture.org")
-;;       (file "~/.emacs.d/orgfiles/tpl-task.txt"))
-;;      ("c" "Add code" entry
-;;       (file "~/.emacs.d/orgfiles/code.org")
-;;       (file "~/.emacs.d/orgfiles/tpl-code.txt"))))
-;;  '(org-directory "~/.emacs.d/orgfiles/")
-;;  '(org-outline-path-complete-in-steps nil)
-;;  '(org-refile-allow-creating-parent-nodes t)
-;;  '(org-refile-targets '((org-agenda-files :maxlevel . 2)))
-;;  '(org-refile-use-outline-path 'file)
-;;  '(package-selected-packages
-;;    '(yapfify company-box company lsp-ivy zenburn-theme which-key vterm use-package symon request-deferred popup poly-R pdf-tools ox-gfm org-roam org-bullets multi-term modus-vivendi-theme modus-operandi-theme magit lsp-ui lsp-python-ms lsp-pyright lsp-julia latex-preview-pane latex-math-preview jupyter git-gutter flymd flycheck fireplace exec-path-from-shell ess elpy elfeed ein eglot counsel conda company-web company-try-hard company-statistics company-quickhelp company-lsp column-enforce-mode clipetty auctex anki-mode anki-editor anki-connect anaconda-mode ag))
-;;  '(show-paren-mode t)
-;;  '(tool-bar-mode nil)
-;;  '(tramp-encoding-shell "/bin/bash"))
-;; (custom-set-faces
-;;  ;; custom-set-faces was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  '(preview-face ((t (:background "white"))))
-;;  '(preview-reference-face ((t (:background "white")))))
+(set-face-attribute 'default (selected-frame) :height 200)
